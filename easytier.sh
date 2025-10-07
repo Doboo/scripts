e13cd6fec7f544b72e4fc6ab7ba372d9758f55d8
guoke <Doboo@users.noreply.github.com>
2025-10-07 13:15:33 +0800

    Update easytier.sh

--- easytier.sh
+++ easytier.sh
@@ -153,7 +153,19 @@ WantedBy=multi-user.target"
 
     echo "$service_content" > /etc/systemd/system/easytier.service
 
-    # 5. 启动服务
+ 
+  # 5. 启动服务
+  export tun_IF=tun0  && export WAN_IF=eth0  #设置物理网卡和虚拟网卡的接口
+#其中的 tun0 在不同的机器中不一样，你可以在路由器ssh环境中用 ip addr
+  iptables -I FORWARD -i $WAN_IF -j ACCEPT
+  iptables -I FORWARD -o  $WAN_IF -j ACCEPT
+  iptables -t nat -I POSTROUTING -o  $WAN_IF -j MASQUERADE
+  iptables -I FORWARD -i $tun_IF -j ACCEPT
+  iptables -I FORWARD -o $tun_IF -j ACCEPT
+  iptables -t nat -I POSTROUTING -o $tun_IF -j MASQUERADE
+  apt-get install iptables-persistent -y #保存规则，重启后能生效
+  netfilter-persistent save
+   # 5. 启动服务
     systemctl daemon-reload
     systemctl enable easytier
     systemctl restart easytier
